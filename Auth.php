ef1a4322e3945b8e887d88be0a4df09c6f5f8076
Gurdeep Singh (Guru) <oyeaussie@gmail.com>
2026-05-08 23:57:45 +0530

    Initial Commit

---
+++ Auth.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Apps\Tms\Middlewares\Auth;
+
+use System\Base\BaseMiddleware;
+
+class Auth extends BaseMiddleware
+{
+    public function process($data)
+    {
+        //
+    }
+}
