d6632e9be9d3a0d6949189c6036f697a94dcac2a
Gurdeep Singh (Guru) <oyeaussie@gmail.com>
2026-05-27 00:23:55 +0530

    update

--- Auth.php
+++ Auth.php
@@ -8,6 +8,33 @@ class Auth extends BaseMiddleware
 {
     public function process($data)
     {
-        //
+        //Authenticate API
+        if ($this->api->isApi()) {
+            return $this->api->authCheck();
+        }
+
+        // Authenticate if in session
+        if ($this->access->auth->hasUserInSession()) {
+            try {
+                $this->access->auth->setUserFromSession();
+            } catch (\Exception $e) {
+                $this->access->auth->logout();
+            }
+        }
+
+        //Authenticate via Cookie
+        if ($this->access->auth->hasRecaller()) {
+            try {
+                $this->access->auth->setUserFromRecaller();
+            } catch (\Exception $e) {
+                $this->access->auth->logout();
+            }
+        }
+
+        //Authenticated
+        if (!$this->access->auth->check()) {
+            $this->session->set('redirectUrl', $this->request->getUri());
+            return $this->response->redirect($data['appRoute'] . '/auth');
+        }
     }
 }
